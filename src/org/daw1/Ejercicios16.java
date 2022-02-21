15baf52834c6911ccbe1feef271499feff6a4031
silviotron <silvionovasmartinezgmail.com>
2022-02-21 18:28:05 +0100

    upgrade

--- src/org/daw1/Ejercicios16.java
+++ src/org/daw1/Ejercicios16.java
@@ -5,17 +5,98 @@
  */
 package org.daw1;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Set;
+import java.util.TreeSet;
+import org.daw1.clasess.Alumno;
 /**
  *
  * @author alumno
  */
 public class Ejercicios16 {
-
+    private static Set<Alumno> almacen = new TreeSet<>();
+    private static java.util.Scanner sc;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String seleccion = "";
+        sc = new java.util.Scanner(System.in);
+        do {
+            
+            seleccion = "";
+            System.out.println("*************************");
+            System.out.println("*1. Crear alumno        *");
+            System.out.println("*2. Modificar alumno    *");
+            System.out.println("*3. Mostrar alumnos     *");
+            System.out.println("*0. Salir               *");
+            System.out.println("*************************");
+            seleccion = sc.nextLine();
+            switch(seleccion){
+                case "1":
+                    crear();
+                    break;
+                case "2":
+                    modificar();
+                    break;
+                case "3":
+                    mostrar();
+                    break;
+                case "0":
+                    break;
+                default:
+                    System.out.println("opcion incorrecta");
+                    
+            }
+            
+        } while(!seleccion.equals("0"));
     }
     
+    public static void crear(){
+        String dni = "";
+        do {            
+            System.out.print("Introduce el dni del alumno: ");
+            dni = sc.nextLine().toUpperCase();
+        } while (!dni.matches("[1-9]{8}[A-Z]"));
+        System.out.print("Introduce nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("Introduce Apellido: ");        
+        String apellidos = sc.nextLine();
+        LocalDate fecha = pedirFecha();
+        almacen.add(alumno(nombre, apellidos, dni, fecha));
+        
+        
+
+    }
+    public static LocalDate pedirFecha(){
+        LocalDate fecha = null;
+        do{            
+            try{            
+                System.out.print("Introduce dia de nacimiento: ");
+                int dia = sc.nextInt();
+                System.out.print("Introduce mes de nacimiento: ");
+                int mes = sc.nextInt();
+                System.out.print("Introduce año de nacimiento: ");
+                int ano = sc.nextInt();
+                fecha = LocalDate.of(ano,mes,dia);
+                
+            } catch (DateTimeException e){
+                System.out.println("fecha no valida");
+            } catch (InputMismatchException e){
+                System.out.println("debe introducir un numero");
+            }
+            sc.nextLine();
+        }while(fecha == null);
+        return fecha;
+    }
+    public static void modificar(){
+        
+    }
+    public static void mostrar(){
+        System.out.println(Arrays.toString(almacen.toArray()));
+    }
 }
--- src/org/daw1/clasess/Alumno.java
+++ src/org/daw1/clasess/Alumno.java
@@ -11,15 +11,16 @@ import java.time.LocalDate;
  *
  * @author alumno
  */
-public class Alumno {
+public class Alumno implements Comparable<Alumno>{
+
     private String nombre;
     private String apellidos;
     private final String dni;
     private final LocalDate fechaNacimiento;
     private Integer notaPrimero = null;
     private Integer notaSegundo = null;
-    
-    public Alumno(String nombre, String apellidos, String dni, LocalDate fechaNacimiento){
+
+    public Alumno(String nombre, String apellidos, String dni, LocalDate fechaNacimiento) {
         checkNotNull(nombre);
         checkNotNull(apellidos);
         checkDni(dni);
@@ -28,98 +29,159 @@ public class Alumno {
         this.apellidos = apellidos;
         this.dni = dni;
         this.fechaNacimiento = fechaNacimiento;
-        
+
     }
-    
-    private static <E> void checkNotNull(E cadena){
-        if(cadena == null){
+
+    private static <E> void checkNotNull(E cadena) {
+        if (cadena == null) {
             throw new IllegalArgumentException("no se permiten valores nulos");
         }
-        
+
     }
-    
+
     /**
-     * Suponemos que un DNI debe tener siempre 8 números seguidos de una letra mayúscula
-     * @param dni 
+     * Suponemos que un DNI debe tener siempre 8 números seguidos de una letra
+     * mayúscula
+     *
+     * @param dni
      */
-    private static void checkDni(String dni){
-        if(dni == null || !dni.matches("[0-9]{8}[A-Z]")){
+    private static void checkDni(String dni) {
+        if (dni == null || !dni.matches("[0-9]{8}[A-Z]")) {
             throw new IllegalArgumentException("El DNI debe estar formado por 8 números seguidos de una letra");
         }
-        
+
     }
-    
+
     /**
      * Comprueba que la fecha de nacimiento es anterior a la fecha actual
-     * @param fechaNacimiento 
+     *
+     * @param fechaNacimiento
      */
-    private static void checkFechaNacimiento(LocalDate fechaNacimiento){
-        if(fechaNacimiento == null || fechaNacimiento.compareTo(LocalDate.now()) >= 0){
+    private static void checkFechaNacimiento(LocalDate fechaNacimiento) {
+        if (fechaNacimiento == null || fechaNacimiento.compareTo(LocalDate.now()) >= 0) {
             throw new IllegalArgumentException("La fecha de nacimiento debe ser anterior a hoy");
-            
+
         }
-        
+
     }
-    private static void checkNota(Integer nota){
-        if(nota != null && (nota < 0 || nota > 10)){
+
+    private static void checkNota(Integer nota) {
+        if (nota != null && (nota < 0 || nota > 10)) {
             throw new IllegalArgumentException("la nota debe tener un valor entre 0 y 10");
         }
     }
-    
-    public String getNombre(){
+
+    public String getNombre() {
         return this.nombre;
     }
-    
-    public String getApellidos(){
+
+    public String getApellidos() {
         return this.apellidos;
     }
-    
-    public String getDni(){
+
+    public String getDni() {
         return this.dni;
     }
-    
-    public LocalDate getFechaNacimiento(){
+
+    public LocalDate getFechaNacimiento() {
         return this.fechaNacimiento;
     }
-    
-    public Integer getNotaPrimero(){
+
+    public Integer getNotaPrimero() {
         return this.notaPrimero;
     }
-    
-    public Integer getNotaSegundo(){
+
+    public Integer getNotaSegundo() {
         return this.notaSegundo;
     }
-    
-    public void setNombre(String nombre){
-        checkNotNull(nombre);        
+
+    public void setNombre(String nombre) {
+        checkNotNull(nombre);
         this.nombre = nombre;
     }
-    
-    public void setApellidos(String apellidos){
+
+    public void setApellidos(String apellidos) {
         checkNotNull(apellidos);
         this.apellidos = apellidos;
     }
-    
-    public void setNotaPrimero(Integer nota){
+
+    public void setNotaPrimero(Integer nota) {
         checkNota(nota);
         this.notaPrimero = nota;
     }
-    
+
     /**
      * Establece la nota de segundo para el alumno
-     * @param nota nota de segundo para el alumno 
-     * @throws 
+     *
+     * @param nota nota de segundo para el alumno
+     * @throws
      */
-    public void setNotaSegundo(Integer nota){
+    public void setNotaSegundo(Integer nota) {
         checkNota(nota);
         this.notaSegundo = nota;
     }
-    
+
     /**
-     * Devuelve el nombre completo, resultado de concatenar el nombre y los apellidos
-     * @return nombre completo, resultado de concatenar el nombre y los apellidos 
+     * Devuelve el nombre completo, resultado de concatenar el nombre y los
+     * apellidos
+     *
+     * @return nombre completo, resultado de concatenar el nombre y los
+     * apellidos
      */
-    public String getNomnbreCompleto(){
+    public String getNomnbreCompleto() {
         return this.nombre + " " + this.apellidos;
     }
+
+    /**
+     * Calcula la media del ciclo. En caso de que no tengamos alguna nota
+     * devuelve null
+     *
+     * @return la media del ciclo. En caso de no tener nota en primero o en
+     * segundo devuelve null
+     */
+    public Double getNotaMediaCiclo() {
+        if (this.notaPrimero == null || this.notaSegundo == null) {
+            return null;
+        } else {
+            return (this.notaPrimero + this.notaSegundo) / 2.0d;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        String formatoFecha = "dd/MM/yyyy";
+        java.time.format.DateTimeFormatter formateador = java.time.format.DateTimeFormatter.ofPattern(formatoFecha);
+        return this.dni + " " + this.getNomnbreCompleto() + " " + formateador.format(fechaNacimiento);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+
+        }
+        if (obj instanceof Alumno) {
+            return this.dni.equals(((Alumno) obj).getDni());
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return this.dni.hashCode(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public int compareTo(Alumno t){
+        if(t == null){
+            throw new NullPointerException();
+        }else{
+            return this.dni.compareTo(t.getDni());
+        }
+        
+    }
+
 }
+        
